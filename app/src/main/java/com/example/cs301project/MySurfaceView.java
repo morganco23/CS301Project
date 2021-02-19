0efb48fe30dfae3cfceace7dae029d1379434b0e
connieboyboy <connormrgn40@gmail.com>
2021-02-18 16:58:05 -0800

    Added some chess pieces and removed the top bar

--- app/src/main/java/com/example/cs301project/MySurfaceView.java
+++ app/src/main/java/com/example/cs301project/MySurfaceView.java
@@ -4,14 +4,20 @@ package com.example.cs301project;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.ColorSpace;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
 import android.view.SurfaceView;
 
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.BitmapCompat;
 
 public class MySurfaceView extends SurfaceView {
@@ -28,13 +34,26 @@ public class MySurfaceView extends SurfaceView {
         red.setColor(Color.RED);
 
 
+
         Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.chessboard);
 
+        int w = canvas.getWidth();
+
+        image = Bitmap.createScaledBitmap(image,w,w, false);
+
+        Bitmap whiteRook = BitmapFactory.decodeResource(getResources(), R.drawable.whiterookpng);
+        int rookwidth = w/9;
+        int rookheight = w/9 * whiteRook.getHeight()/whiteRook.getWidth();
+        whiteRook = Bitmap.createScaledBitmap(whiteRook,rookwidth,rookheight,false);
 
 
         Paint black = new Paint();
         black.setColor(Color.BLACK);
-        canvas.drawBitmap(image, 0.f, 0.f, black);
+        ColorFilter blackFilter = new PorterDuffColorFilter(ContextCompat.getColor(this.getContext(), R.color.black), PorterDuff.Mode.SRC_IN);
+        black.setColorFilter(blackFilter);
+        canvas.drawBitmap(image, 0.f, 0.f, null);
 
+        canvas.drawBitmap(whiteRook,0,0,null);
+        canvas.drawBitmap(whiteRook, w-w/8,0,black);
     }
 }
